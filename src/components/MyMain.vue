55e5460339254bc2f093ae3c0e176f33343da97a
Maurizio <proiettomaurizio97@gmail.com>
2021-12-01 11:23:55 +0100

    i create fil vue with macrostructure and debug. Inastall axios

---
+++ src/components/MyMain.vue
@@ -0,0 +1,37 @@
+<template>
+  <main>
+      <div class="container-card">
+          <div class="card">
+
+          </div>
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MyMain',
+  created() {
+
+  },
+  methods: {
+      getArray(){
+          
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    main {
+        margin: 0 auto;
+        width: 70%;
+    }
+    .container-card {
+        // debug
+        height: 200px;
+        width: 100px;
+        background-color: blueviolet;
+    }
+</style>
