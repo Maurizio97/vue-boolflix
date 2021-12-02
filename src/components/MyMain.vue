aee28dfeb11b4d607111665a459a45334eee4c1b
Maurizio <proiettomaurizio97@gmail.com>
2021-12-02 11:17:48 +0100

    milestone 2 complete

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,18 +1,20 @@
 <template>
   <main>
-      <div class="container-card" v-for="film, i in listFilm" :key="i">
+    <!-- film -->
+      <div class="container-card" v-for="film in listFilm" :key="film.id">
           <div class="card">
             <div> Titolo: {{film.title}}</div>
             <div>Titolo Originale: {{film.original_title}}</div>
-            <div>Lingua: {{film.original_language}}</div>
+            <div>Lingua: <img :src="insertFlag(film.original_language)"></div>
             <div>Voto: {{film.vote_average}}</div>
           </div>
       </div>
+      <!-- serie tv -->
       <div class="container-card" v-for="tv in listTv" :key="tv.id">
           <div class="card">
             <div> Titolo: {{tv.name}}</div>
             <div>Titolo Originale: {{tv.original_name}}</div>
-            <div>Lingua: {{tv.original_language}}</div>
+            <div>Lingua: <img :src="insertFlag(tv.original_language)"></div>
             <div>Voto: {{tv.vote_average}}</div>
           </div>
       </div>
@@ -27,6 +29,26 @@ export default {
       listFilm:Array,
       listTv:Array,
   },
+  data(){
+    return {
+      itFlag: require("@/assets/it_flag.png"),
+      enFlag: require("@/assets/en_flag.png"),
+      frFlag: require("@/assets/fr_flag.png"),
+    }
+  },
+  methods: {
+    insertFlag(item){
+      if(item === "it"){
+        return this.itFlag
+      } else if (item === "en"){
+        return this.enFlag
+      } else if (item === "fr"){
+        return this.frFlag
+      } else {
+        return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvUgkbNyXbD7bXamCmiGz2ZBAVbyaU_fvARQ&usqp=CAU"
+      }
+    }
+  }
 }
 </script>
 
@@ -55,5 +77,9 @@ export default {
             align-items: center;
             justify-content: center;
             height: 100%;
+
+            img {
+              height: 10px;
+            }
         }
 </style>
